6457849e78c75b373628ffc96cf055c661a6c757
JulianBlohm <julianscript@outlook.de>
2021-01-08 14:21:44 +0100

    Add ionic textarea to chat page

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -1,18 +1,20 @@
 import React, { useState } from 'react'
-import { IonTextarea, IonItem, IonItemDivider } from '@ionic/react'
+import { IonTextarea, IonItem } from '@ionic/react'
 
 export const TextArea: React.FC = () => {
     const [text, setText] = useState<string>()
 
     return (
         <>
-            <IonItemDivider>Default textarea</IonItemDivider>
             <IonItem>
                 <IonTextarea
                     value={text}
                     onIonChange={(e) => setText(e.detail.value!)}
+                    placeholder="Nachricht"
                 ></IonTextarea>
             </IonItem>
         </>
     )
 }
+
+export default TextArea
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react'
-import ChatInput from '../components/ChatInput'
+import TextArea from '../components/TextArea'
 import styled from 'styled-components/macro'
 
 const Tab1: React.FC = () => {
@@ -11,21 +11,11 @@ const Tab1: React.FC = () => {
                     <IonTitle>Chat</IonTitle>
                 </IonToolbar>
             </IonHeader>
-            <InputWrapper>
-                <ChatInput />
-            </InputWrapper>
+            
+            <TextArea/>
 
-            <IonHeader collapse="condense">
-                <IonToolbar>
-                    <IonTitle size="large">Tab 1</IonTitle>
-                </IonToolbar>
-            </IonHeader>
         </IonPage>
     )
 }
-
-const InputWrapper = styled.div`
-
-`
 
 export default Tab1
