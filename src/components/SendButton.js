236495a526d4f7378855b1595ec16553aa85bb36
JulianBlohm <julianscript@outlook.de>
2021-01-08 15:56:45 +0100

    Create SendButton

---
+++ src/components/SendButton.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components/macro'
+import { IonIcon, IonButton } from '@ionic/react';
+
+
+function SendButton() {
+
+    return (
+        <ion-button>
+            <ion-icon name="paper-plane-outline"></ion-icon>
+        </ion-button>
+    )
+}
+
+export default SendButton
