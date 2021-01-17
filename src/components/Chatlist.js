8ea630f4b840c545c779a00b5ee94a0af07d52a2
JulianBlohm <julianscript@outlook.de>
2021-01-17 12:54:10 +0100

    Rearrange chatlist as list with native message sizing

--- src/components/Chatlist.js
+++ src/components/Chatlist.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useRef } from 'react'
 import styled from 'styled-components/macro'
 
-function Chatlist({chat}) {
-
-    useEffect(() => focusNewMessage(),[chat])
+function Chatlist({ chat }) {
+    useEffect(() => focusNewMessage(), [chat])
 
     const messages = useRef([])
 
@@ -12,34 +11,49 @@ function Chatlist({chat}) {
     function focusNewMessage() {
         messages.current[messages.current.length - 1].scrollIntoView(false)
     }
-    
+
     return (
         <MessageArea>
-            {chat.map((message, index) => 
-            <Message ref={(element) => messages.current[index] = element} key={message.id}>
-                {message.text}
-            </Message>)}
+            {chat.map((message, index) => (
+                <Message
+                    ref={(element) => (messages.current[index] = element)}
+                    key={message.id}
+                >
+                    <MessageText>{message.text}</MessageText>
+                </Message>
+            ))}
         </MessageArea>
     )
 }
 
-const MessageArea = styled.section`
-display: flex;
-flex-direction: column;
-flex-wrap: nowrap;
-grid-gap: 5px;
-margin-bottom: 60px;
-overflow-y: scroll;
-padding: 10px;
+const MessageArea = styled.ul`
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    grid-template-columns: 1;
+    grid-gap: 5px;
+    margin: 0 0 60px 0;
+    overflow-y: scroll;
+    padding: 10px;
 `
 
-const Message = styled.p`
-margin: 0;
-border-radius: 5px;
-padding: 10px;
-background: #519e1e;
-color: white;
-white-space: pre-wrap;
+const Message = styled.li`
+    margin: 0;
+    border-radius: 5px;
+    color: white;
+    white-space: pre-line;
+    display: inline-block;
 `
+
+const MessageText = styled.p`
+    margin: 0;
+    border-radius: 5px;
+    padding: 10px;
+    background: #519e1e;
+    color: white;
+    white-space: pre-line;
+    display: inline-block;
+`
+
 
 export default Chatlist
